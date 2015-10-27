d26c587a8852cac5fd798778f8743c9b8ac79188
Vince Scott <c0d3x42@gmail.com>
2015-10-27 23:49:56 +0000

    i

--- p839v55-vendor.mk
+++ p839v55-vendor.mk
@@ -22,3 +22,5 @@ $(call inherit-product, vendor/vodafone/p839v55/p839v55-vendor-blobs.mk)
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9306; \
 	ln -sf /data/misc/audio/wcd9320_anc.bin \
 		$(TARGET_OUT_ETC)/firmware/wcd9306/wcd9306_anc.bin; \
+	ln -sf /data/misc/audio/mbhc.bin \
+		$(TARGET_OUT_ETC)/firmware/wcd9306/wcd9306_mbhc.bin)
